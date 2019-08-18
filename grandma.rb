a562f9ff8c09d852ba35f6c7777e38c2c3efa387
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-18 15:13:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,8 @@
 def speak_to_grandma(text)
   if text == text.upcase
     puts "NO, NOT SINCE 1938!"
-  elsif text!= nil 
-    puts "HUH?! SPEAK UP, SONNY!"  
+  elsif text == "I LOVE YOU GRANDMA!" 
+    puts "I LOVE YOU TOO PUMPKIN!"  
   else
     puts "HUH?! SPEAK UP, SONNY!"
 end 
